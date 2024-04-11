dc66d67a78de555dcefd4962e938d30a5692c232
JONATAN_MAZUCA_VALENZUELA <a.21308051280382@cbtis128.edu.mx>
2024-04-11 17:46:34 +0000

    act

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:mazuca0382/BNavigation/routes.dart';
+import 'BNavigation/bottom_nav.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Myapp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class Myapp extends StatefulWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return const MaterialApp(
+        title: "Material App",
+        debugShowCheckedModeBanner: false,
+        home: HomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  BNavigator? myBNB;
+
+  @override
+  void initState() {
+    myBNB = BNavigator(currentIndex: (i) {
+      setState(() {
+        index = i;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      bottomNavigationBar: myBNB,
+      body: Routes(index: index),
     );
   }
 }
